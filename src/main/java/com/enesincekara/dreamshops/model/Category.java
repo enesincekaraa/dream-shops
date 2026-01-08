c032c0579664de1f112cb65b8f83912aae377a1d
enesincekaraa <enesincekara61@gmail.com>
2026-01-08 10:52:43 +0300

    chore: load env from .env and tweak logging

---
+++ src/main/java/com/enesincekara/dreamshops/model/Category.java
@@ -0,0 +1,26 @@
+package com.enesincekara.dreamshops.model;
+
+import jakarta.persistence.OneToMany;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
