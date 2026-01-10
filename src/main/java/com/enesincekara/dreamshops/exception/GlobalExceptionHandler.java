6e018a2d17a31a5197f62d92a71069c79843e98a
enesincekaraa <enesincekara61@gmail.com>
2026-01-10 21:35:12 +0300

    feat:Added validation exceptions

--- src/main/java/com/enesincekara/dreamshops/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enesincekara/dreamshops/exception/GlobalExceptionHandler.java
@@ -4,12 +4,16 @@ package com.enesincekara.dreamshops.exception;
 import com.enesincekara.dreamshops.exception.category.CategoryNotFoundException;
 import com.enesincekara.dreamshops.exception.product.ProductNotFoundException;
 import com.enesincekara.dreamshops.response.ErrorResponse;
+import com.enesincekara.dreamshops.response.ValidationErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -40,6 +44,25 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
         return new ErrorResponse(
                 e.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return new ValidationErrorResponse(
+                "Validation failed",
+                errors,
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
--- src/main/java/com/enesincekara/dreamshops/request/StockUpdateRequest.java
+++ src/main/java/com/enesincekara/dreamshops/request/StockUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.enesincekara.dreamshops.request;
 
+import jakarta.validation.constraints.PositiveOrZero;
+
 public record StockUpdateRequest(
+        @PositiveOrZero(message = "Quantity must be greater than zero")
         int quantity
 ) {
 }
